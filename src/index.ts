bb11a809146d8464dce70826b56a2a78376be3f2
Gautier <gautier@apparence.io>
2022-06-15 18:45:44 +0200

    fix calling plugin from browser

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,62 @@
-import {HttpClient} from './api/httpclient';
-import {LocalstorageService} from './api/localstorage';
+import { HttpClient } from './api/httpclient';
+import { LocalstorageService } from './api/localstorage';
 import {
-  PalSession,
-  PalSessionRequest,
-  PlatformTypes,
+    PalSession,
+    PalSessionRequest,
+    PlatformTypes,
 } from './api/models/session.model';
-import {SessionsApi} from './api/sessions.api';
-import {PalOptions} from './options';
+import { SessionsApi } from './api/sessions.api';
+import { PalOptions } from './options';
 
-export default class Pal {
-  private options!: PalOptions;
+export class Pal {
+    private options!: PalOptions;
 
-  private hasInitialized = false;
+    private hasInitialized = false;
 
-  private static _instance: Pal | null;
+    private static _instance: Pal | null;
 
-  static getInstance(): Pal {
-    if (!this._instance) {
-      throw new Error(
-        'Pal has not been initialized, please call createInstance'
-      );
+    static getInstance(): Pal {
+        if (!this._instance) {
+            throw new Error(
+                'Pal has not been initialized, please call createInstance'
+            );
+        }
+        return this._instance;
     }
-    return this._instance;
-  }
 
-  static createInstance(options: PalOptions) {
-    const httpClient = new HttpClient({
-      baseUrl: options.serverUrl,
-      apiKey: options.apiKey,
-    });
-    const sessionsApi = new SessionsApi(new LocalstorageService(), httpClient);
-    this._instance = new Pal(sessionsApi, options);
-  }
-
-  constructor(private sessionsApi: SessionsApi, options: PalOptions) {
-    this.options = options;
-  }
-
-  async initialize(): Promise<void> {
-    await this.sessionsApi.createSession(<PalSessionRequest>{
-      frameworkType: 'JAVASCRIPT',
-      platform: this.options.platform ?? PlatformTypes.web,
-    });
-    this.hasInitialized = true;
-  }
-
-  async getSession(): Promise<PalSession | null> {
-    if (!this.hasInitialized) {
-      throw new Error('Pal has not been initialized');
+    static createInstance(options: PalOptions): Pal {
+        const httpClient = new HttpClient({
+            baseUrl: options.serverUrl,
+            apiKey: options.apiKey,
+        });
+        const sessionsApi = new SessionsApi(new LocalstorageService(), httpClient);
+        this._instance = new Pal(sessionsApi, options);
+        return this._instance;
     }
-    return this.sessionsApi.getSession();
-  }
 
-  async clearSession(): Promise<void> {
-    if (!this.hasInitialized) {
-      throw new Error('Pal has not been initialized');
+    constructor(private sessionsApi: SessionsApi, options: PalOptions) {
+        this.options = options;
     }
-    await this.sessionsApi.clearSession();
-  }
+
+    async initialize(): Promise<void> {
+        await this.sessionsApi.createSession(<PalSessionRequest>{
+            frameworkType: 'JAVASCRIPT',
+            platform: this.options.platform ?? PlatformTypes.web,
+        });
+        this.hasInitialized = true;
+    }
+
+    async getSession(): Promise<PalSession | null> {
+        if (!this.hasInitialized) {
+            throw new Error('Pal has not been initialized');
+        }
+        return this.sessionsApi.getSession();
+    }
+
+    async clearSession(): Promise<void> {
+        if (!this.hasInitialized) {
+            throw new Error('Pal has not been initialized');
+        }
+        await this.sessionsApi.clearSession();
+    }
 }
