3897c1a4414fd8a8d1f94919e8fa1ddefb7dc556
Gautier <gautier@apparence.io>
2022-06-17 15:54:15 +0200

    refactoring pal sdk

--- src/sdk/palsdk.ts
+++ src/sdk/palsdk.ts
@@ -6,7 +6,7 @@ export interface ShowVideoOnlyParams {
     minVideoUrl: string;
     userName: string;
     companyTitle: string;
-    avatarUrl?: string;
+    // avatarUrl?: string;
     onExpand?: Function;
     onVideoEnd?: Function;
     onSkip?: Function;
@@ -27,70 +27,42 @@ export class PalSdk {
         const minVideoUrl = params.minVideoUrl;
         const userName = params.userName;
         const companyTitle = params.companyTitle;
-        const avatarUrl = params.avatarUrl;
+        // const avatarUrl = params.avatarUrl;
         const onExpand = params.onExpand;
         const onVideoEnd = params.onVideoEnd;
         const onSkip = params.onSkip;
-        this.showVideoMiniature({
-            minVideoUrl,
-            videoUrl,
-            userName,
-            companyTitle,
-            onExpand: onExpand ?? (() => { }),
+        const onVideoEndAction = (() => {
+            if (onVideoEnd) {
+                onVideoEnd();
+            }
         });
-    }
+        const onSkipAction = (() => {
+            if (onSkip) {
+                onSkip();
+            }
+        });
+        const onVideoExpandedAction = (() => {
+            if (onExpand) {
+                onExpand();
+            }
+            const videoExpanded = new PalVideoExpanded(
+                userName,
+                companyTitle,
+                videoUrl,
+                onSkipAction,
+                onVideoEndAction,
+                true
+            );
+            videoExpanded.appendToDom();
 
-    /// PRIVATES
+        });
 
-    private async showVideoMiniature(
-        { minVideoUrl, videoUrl, userName, companyTitle, onExpand }: {
-            minVideoUrl: string,
-            videoUrl: string,
-            userName: string,
-            companyTitle: string,
-            onExpand: Function,
-        }
-    ) {
         const videoMiniature = new PalVideoMiniature(
             minVideoUrl,
             150,
-            () => {
-                if (onExpand) {
-                    onExpand();
-                }
-                this.showVideoExpanded({
-                    videoUrl,
-                    userName,
-                    companyTitle,
-                    closeAfterVideoEnd: true,
-                    onVideoEndAction: () => { },
-                    onClose: () => { }
-                });
-            },
+            onVideoExpandedAction
         );
         videoMiniature.appendToDom();
-
-    }
-
-    private async showVideoExpanded(
-        { videoUrl, userName, companyTitle, closeAfterVideoEnd, onVideoEndAction, onClose }: {
-            videoUrl: string,
-            userName: string,
-            companyTitle: string,
-            closeAfterVideoEnd: boolean,
-            onVideoEndAction: Function,
-            onClose: Function,
-        }
-    ) {
-        const videoExpanded = new PalVideoExpanded(
-            userName,
-            companyTitle,
-            videoUrl,
-            onClose,
-            onVideoEndAction,
-            closeAfterVideoEnd
-        );
-        videoExpanded.appendToDom();
     }
 
 
